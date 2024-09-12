6d831df2d909d6178f5a2bb2f9fdd92c8cbdc646
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-09-12 15:22:05 -0300

    Java coding practice

--- exrept05.java
+++ exrept05.java
@@ -10,40 +10,49 @@ public class exrept05 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+        
+		//variável booleana que serve para entrar e sair do loop while 
+		
 		boolean cont = true;
-
+		
+		//variáveis que servem para contar a quantidade de combustiveis selecionados (a = Alcool; g = Gasolina; d = Diesel)
+		
 		int a = 0;
 		int g = 0;
 		int d = 0;
-		int opcao = 0;
-
+		
+		//loop while que exibe o menu com opçoes de combustiveis a serem selecionados e a opção de sair do loop (opção 4)
 		while (cont) {
-			System.out.println("informe qual tipo de combustivel você mais usa: ");
+			System.out.println("\ninforme qual tipo de combustivel você mais usa: ");
 			System.out.println("1.Álcool");
 			System.out.println("2.Gasolina");
 			System.out.println("3.Diesel");
 			System.out.println("4.Fim\n");
-			opcao = sc.nextInt();
-
-			if (opcao == 4) {
-				System.out.println("MUITO OBRIGADO");
-				System.out.printf("Alcool: %d%nGasolina: %d%nDiesel: %d%n", a, g, d);
-				cont = false;
-
-			} else if (opcao == 1) {
+			
+			//variável que recebe um valor inteiro informado pelo usuário
+			System.out.print(">>>");
+			int opcao = sc.nextInt();
+			
+		  //estrutura condicional que determina qual condição será executada baseando-se na entrada do usuário na variavel 'opcao' 
+			
+			if (opcao == 1) {
 				a += 1;
-				
 			} else if (opcao == 2) {
 				g += 1;
-			
 			} else if (opcao == 3) {
 				d += 1;
+			} else if (opcao == 4) {
+				System.out.println("MUITO OBRIGADO!");
+				System.out.printf("Alcool: %d%nGasolina: %d%nDiesel: %d", a, g, d);
+				cont = false;
 				
-			} else {				
-			
+			//se por acaso uma opção que não encaixe no intervalo de 1 a 4 for digitada pelo usuário (ex: 5), a condicional else 
+			//será executada e retornara ao loop imprimindo a seguinte mensagem "Insira uma opção válida, insira um número inteiro de 1 a 4"
+				
+			} else {
+				System.out.println("Insira uma opção válida, insira um número inteiro de 1 a 4\n");
 			}
-			
+
 		}
 
 		sc.close();
