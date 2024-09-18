b06406ff8cf8e8049c053f3354e4ddb545762a87
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-09-18 20:39:47 -0300

    Update exfor02.java

--- exfor02.java
+++ exfor02.java
@@ -15,8 +15,25 @@ public class exfor02 {
 		sc.nextLine();
 		int y = sc.nextInt();
 		
+		int min, max;
 		
+		if (x < y) {
+			min = x;
+			max = y;
+	    }
+		else {
+		   min = y;
+		   max = x;
+	   }
+		int soma = 0;
+		for (int i=min+1; i<max; i++) {
+			if (i % 2 != 0) {
+				soma += i;
+			}
+		}
 		
+		System.out.println(soma);
+						
 		sc.close();
 	
 	}
