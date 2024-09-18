ad8909b1b4407a18b58bc95062843abc9d1c3a0b
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-09-18 16:05:15 -0300

    Java coding practice

---
+++ exfor02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//leia valores inteiros x e y. A seguir, calcule e 
+//mostre a soma dos números impares entre eles.
+//O programa deve imprimir um valor inteiro. Este valor
+//é a soma dos valores ímpares que estão entre os valores
+//fornecidos na entrada que deverá caber em um inteiro.
+
+public class exfor02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		sc.nextLine();
+		int y = sc.nextInt();
+		
+		
+		
+		sc.close();
+	
+	}
+		
+		
+	}
