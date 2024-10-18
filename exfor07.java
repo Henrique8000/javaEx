d8c49a2e5f8c3575c1f6f879649750ec0ecbb786
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-10-18 20:37:59 -0300

    Update exfor07.java

--- exfor07.java
+++ exfor07.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 //Lembrando que, por definição, fatorial de 0 é 1.
 
 public class exfor07 {
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-			
+		int n = sc.nextInt();
+		int total = 1;
+		
+		for (int i = n; i>0; i--) {	
+			 total *= i;  
+			System.out.println(total);
+		}
+		
 		sc.close();
 	}
-
 }
