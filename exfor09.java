b4d0e6a4e233ab7b9a3f25b46e61b0d4ee69ac3d
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2024-10-23 17:54:31 -0300

    Java coding practice
    Programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
    começando de 1 até N. . Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor,
    conforme exemplo.

---
+++ exfor09.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+//Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+//começando de 1 até N. . Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor,
+//conforme exemplo.
+
+public class exfor09 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Insira um número inteiro positivo: ");
+		int n = sc.nextInt();
+		
+		for(int line=1; line<n+1; line++) {
+			int pot2 = (int) Math.pow(line, 2);
+			int pot3 = (int) Math.pow(line, 3);
+			System.out.printf("%d %d %d%n", line, pot2, pot3);
+		}
+		
+		sc.close();
+
+	}
+
+}
