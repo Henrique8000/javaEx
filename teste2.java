4daa4e2caeaad97e1e7933652aa9bb93611aca70
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-01-05 03:22:09 -0300

    Using javax swing

---
+++ teste2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+
+public class teste2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		JOptionPane.showMessageDialog(null, "Oi");
+		
+		sc.close();
+	}
+	
+	
+	
+}
