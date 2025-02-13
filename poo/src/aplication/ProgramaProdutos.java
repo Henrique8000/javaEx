57a1ac7fe227924d77b943bdafc0b4d50ffc2249
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-02-13 17:36:57 -0300

    Update ProgramaProdutos.java

--- poo/src/aplication/ProgramaProdutos.java
+++ poo/src/aplication/ProgramaProdutos.java
@@ -3,48 +3,45 @@ package aplication;
 import java.util.Locale;
 import java.util.Scanner;
 
-import	entities.Product;
+import entities.Product;
 
 public class ProgramaProdutos {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		Product product = new Product();
-	
-		
+
 		System.out.println("Enter porduct data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
-		
+		String name = sc.nextLine();
+
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		
-		System.out.print("Quantityy in stock: ");
-		product.quantity = sc.nextInt();
-		
-		
-		System.out.println("Product data: " + product);
-		
-		System.out.println();
-		
-		System.out.print("Enter the number of products to be added in stock: ");
+		double price = sc.nextDouble();
+
+		System.out.print("Quantity in stock: ");
 		int quantity = sc.nextInt();
-		product.addProducts(quantity);
-		
+		Product product = new Product(name, price, quantity);
+
+		System.out.println("Product data: " + product);
+
 		System.out.println();
-		
+
+		System.out.print("Enter the number of products to be added in stock: ");
+		quantity = sc.nextInt();
+		product.addProducts(quantity);
+
+		System.out.println();
+
 		System.out.println("Updated data: " + product);
-		
+
 		System.out.println();
 		System.out.print("Enter the number of products to be removed from stock: ");
 		quantity = sc.nextInt();
 		product.removeProducts(quantity);
-		
+
 		System.out.println();
-	    System.out.println("Uptated data: " + product);
-		
+		System.out.println("Uptated data: " + product);
+
 		sc.close();
 
 	}
