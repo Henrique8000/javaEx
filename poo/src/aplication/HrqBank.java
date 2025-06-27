9aa55842fa2d003eef1e22c2cea97fabadd5f6ec
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-06-27 00:17:27 -0300

    Update HrqBank.java
    imported class customer

--- poo/src/aplication/HrqBank.java
+++ poo/src/aplication/HrqBank.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Scanner;
 import java.util.Locale;
 
-import entities.Bank;
+import entities.Customer;
 
 public class HrqBank {
 
@@ -22,37 +22,37 @@ public class HrqBank {
 		System.out.print("Is there an initial deposit (y/n)? ");
 		String isInitial = sc.nextLine();
 		
-		Bank bank;
+		Customer customer;
 		
 		if (isInitial.equals("y")) {
 			System.out.print("Enter initial deposit value: ");
 			double initialDeposit = sc.nextDouble();
-			bank = new Bank(accountNumber, accountName, initialDeposit);
+			customer = new Customer(accountNumber, accountName, initialDeposit);
 			System.out.println("\nAccount data:");
-			System.out.println(bank);
+			System.out.println(customer);
 		}
 		
 		else {
-			bank = new Bank(accountNumber, accountName);
+			customer = new Customer(accountNumber, accountName);
 			System.out.println("\nAccount data:");
-			System.out.println(bank);
+			System.out.println(customer);
 		}
 		
 		System.out.print("\nEnter a deposit value: ");
 		double money = sc.nextDouble();
 		
-		bank.deposit(money);
+		customer.deposit(money);
 		System.out.println("Updated account data:");
-		System.out.println(bank);
+		System.out.println(customer);
 		
 		sc.nextLine();
 		
 		System.out.print("\nEnter a withdraw value: ");
 		double moneOut = sc.nextDouble();
 		
-		bank.withdraw(moneOut);
+		customer.withdraw(moneOut);
 		System.out.println("Updated account data:");
-		System.out.println(bank);
+		System.out.println(customer);
 		
 		sc.close();
 	}
