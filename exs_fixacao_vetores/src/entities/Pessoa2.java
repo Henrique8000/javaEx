a32eba31b3f020bdae97be8c4007293069189ecd
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-08-03 16:28:18 -0300

    add: classe Pessoa2

---
+++ exs_fixacao_vetores/src/entities/Pessoa2.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Pessoa2 {
+
+	private double altura;
+	private char genero;
+	
+	
+	public Pessoa2(double altura, char genero) {
+		this.altura = altura;
+		this.genero = genero;
+	}
+	
+	public double getAltura() {
+		return altura;
+	}
+	
+	public char getGenero() {
+		return genero;
+	}
+}
