988edd01e40edbacad2100d004f6e4da29e47d4b
Henrique <henriqueflavioguimaraes@gmail.com>
2026-01-04 18:17:02 -0300

    add: ex le e escreve arquivo csv

---
+++ ExManipulacaoDeArquivos/src/model/entites/Product.java
@@ -0,0 +1,46 @@
+package model.entites;
+
+public class Product {
+	private String productName;
+	private Double price;
+	private Integer quantity;
+	
+	public Product() {
+	}
+
+	public Product(String productName, Double price, Integer quantity) {
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Double totalPrice() {
+		return quantity * price;
+	}
+	
+	
+}
