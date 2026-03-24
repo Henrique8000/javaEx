3dd968be0ae0fda17260b919ee990c49cc2a733b
Henrique <henriqueflavioguimaraes@gmail.com>
2026-03-24 01:00:40 -0300

    add: modificando servico e aprendende sobre set

---
+++ GenericsSetMap/src/application/ComoTreeSetComparaElementos.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import entities.ProductSet;
+
+
+public class ComoTreeSetComparaElementos {
+	public static void main(String[] args) {
+		Set<ProductSet> set = new TreeSet<>();
+		
+		set.add(new ProductSet("TV", 900.0));
+		set.add(new ProductSet("Notebook", 1200.0));
+		set.add(new ProductSet("Tablet", 400.0));
+		
+		for (ProductSet p : set) {
+			System.out.println(p);
+		}
+	}
+}
