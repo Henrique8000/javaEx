ca121f986d4b554c49adf7182fc23cf59f2c276d
Henrique <henriqueflavioguimaraes@gmail.com>
2026-03-25 00:22:00 -0300

    refact: entitiy to represent log

---
+++ ExSet1/src/entities/LogEntry.java
@@ -0,0 +1,58 @@
+package entities;
+
+import java.time.Instant;
+
+public class LogEntry {
+	
+	private String name;
+	private Instant date;
+	
+	public LogEntry (String name, Instant date) {
+		this.name = name;
+		this.date = date;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LogEntry other = (LogEntry) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
