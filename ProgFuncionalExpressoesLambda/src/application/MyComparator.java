b2cf2238de0670dc3d1b44f4038702b955f83ba0
Henrique <henriqueflavioguimaraes@gmail.com>
2026-03-28 18:27:57 -0300

    feat: aplicando conceitos de prog funcional e lambda
    Usando a interface funcional Comparator e aplicando c classe anonima, funcao anonima, etc

---
+++ ProgFuncionalExpressoesLambda/src/application/MyComparator.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Comparator;
+
+import entities.Product;
+
+// Comparator de objeto de classe separada
+public class MyComparator implements Comparator<Product>{
+
+	@Override
+	public int compare(Product p1, Product p2) {
+		return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
+	}
+		
+}
